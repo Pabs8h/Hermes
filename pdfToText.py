0e03cd3278fbe7b23fee2a402ee5919528b345bf
Pablo Garzón <gapablo2001@gmail.com>
2020-10-25 00:02:12 -0500

    pdf a txt

---
+++ pdfToText.py
@@ -0,0 +1,44 @@
+import PIL.Image
+import pytesseract
+from wand.image import Image as wi
+
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
+
+PDFfile = wi(filename="pdfTest/tutelaTest.pdf",resolution=300)
+Images = PDFfile.convert('jpg')
+ImageSequence = 1
+
+for img in PDFfile.sequence:
+    Image = wi(image = img)
+    Image.save(filename="Image"+str(ImageSequence)+".jpg")
+    print(ImageSequence)
+    ImageSequence += 1
+
+# for page in pages:
+#
+#     filename = "page_" + str(image_counter) + ".jpg"
+#
+#     # Save the image of the page in system
+#     page.save("pdfTest/"+filename, 'JPEG')
+#
+#     # Increment the counter to update filename
+#     image_counter = image_counter + 1
+
+
+filelimit = ImageSequence-1
+
+outfile = "out_text.txt"
+
+f = open(outfile, "a", encoding='utf-8')
+
+# Iterate from 1 to total number of pages
+for i in range(1, filelimit + 1):
+    filename = "image" + str(i) + ".jpg"
+
+    text = str(((pytesseract.image_to_string(PIL.Image.open(filename)))))
+    text = text.replace('-\n', '')
+    print(i)
+    f.write(text)
+    f.write("-----------------------------------------------------------------------------------")
+
+f.close()

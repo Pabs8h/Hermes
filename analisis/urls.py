e2002f2362a6ac1abfb683d3d102b2555e1ab126
Pablo Garzón <gapablo2001@gmail.com>
2020-11-07 17:52:21 -0500

    primer prototipo

--- analisis/urls.py
+++ analisis/urls.py
@@ -2,7 +2,7 @@ from django.urls import  path
 from . import views
 
 urlpatterns= [
-    path('prueba/',views.prueba,name='buscar'),
+    #path('prueba/',views.prueba,name='buscar'),
     path('creacion/',views.creaciontexto,name='creacion'),
     path('analizar/',views.analizar,name='analisis')
 ]
